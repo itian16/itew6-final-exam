56af84ee540cc2646d889c4ba8efbbd223bccb99
RynTrstnRms <ramosryantristan@gmail.com>
2025-05-12 21:00:58 +0800

    API RECONSTRUCT

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -1,13 +1,17 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import BookViewSet, BorrowTransactionViewSet
 
-router = DefaultRouter()
-router.register(r'books', BookViewSet)
-router.register(r'transactions', BorrowTransactionViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    #ENDPOINT NG BOOKS
+    path('books/', BookViewSet.as_view({'get': 'list', 'post': 'create'}), name='book-list'),
+    path('books/<int:pk>/', BookViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='book-detail'),
+    
+    #ENDPOINT NG TRANSACTIONS
+    path('transactions/', BorrowTransactionViewSet.as_view({'get': 'list'}), name='transaction-list'),
+    path('transactions/<int:pk>/', BorrowTransactionViewSet.as_view({'get': 'retrieve'}), name='transaction-detail'),
+    
+    #ENDPOINT NG BORROW
     path('borrow/', BorrowTransactionViewSet.as_view({'post': 'create'}), name='borrow-book'),
     path('return/<int:borrow_id>/', BorrowTransactionViewSet.as_view({'post': 'return_book'}), name='return-book'),
 ]
